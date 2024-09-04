c8c156cc426db9f853b91d180aaa26b32f9c742a
amitchauhan <amitsinghchauhan193@gmail.com>
2024-09-05 01:38:48 +0530

    Reverse the string

--- Practice/src/demo/DemoClass.java
+++ Practice/src/demo/DemoClass.java
@@ -4,7 +4,14 @@ public class DemoClass {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		String name = "Ahmedabad";
+		String reverse = "";
+		
+		for (int i = name.length()-1; i >= 0; i--) {
+			reverse += name.charAt(i);
+		}
+		
+		System.out.println(reverse);
 	}
 
 }
